24606df7c488c8d7c33043ad536a62732ec0f249
Grady Truitt <gradytruitt@gmail.com>
2021-08-03 14:25:34 -0500

    test

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -8,7 +8,6 @@ import * as Styled from "./styles/StyledSearch";
 
 const Search = () => {
   const [selectedCharacterId, setSelectedCharacterId] = React.useState(null);
-  const searchRef = React.useRef();
 
   return (
     <Layout>
@@ -16,8 +15,8 @@ const Search = () => {
         <CharacterGrid characters={heroes} handleSelectCharacter={setSelectedCharacterId} />
       </Styled.GridColumn>
       <Styled.FeaturedColumn fixed>
-        <Styled.SearchInput ref={searchRef} type="search" placeholder="Find a Hero" />
-        <SearchIcon onClick={() => searchRef.current.focus()} />
+        <Styled.SearchInput type="search" placeholder="Find a Hero" />
+        <SearchIcon onClick={() => null} />
         <Styled.LineAccent />
         <FeaturedCharacter id={selectedCharacterId} />
       </Styled.FeaturedColumn>
